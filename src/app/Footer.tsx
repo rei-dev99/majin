95951dcd8692909933bd42bfe0d3fbe1b100a202
rei <idareiya123@gmail.com>
2024-09-24 19:47:48 +0900

    edit:レスポンシブ対応

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="p-4 bg-gradient-to-r from-yellow-500 to-purple-600">
       <div>
-        <p className="text-center text-white">©2024 魔神の王国 - reicode</p>
+        <p className="text-center text-white text-sm md:text-base">©2024 魔神の王国 - reicode</p>
       </div>
     </footer>
   );
--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -5,12 +5,12 @@ function Header() {
   return (
     <header className="flex p-5 justify-between items-center bg-gradient-to-r from-yellow-500 to-purple-600">
       <div>
-        <h1 className="text-3xl font-extrabold text-white">
+        <h1 className="text-xl sm:text-3xl font-extrabold text-white">
           <Link href={"/"}>魔神の挑戦状</Link>
         </h1>
       </div>
       <div>
-        <nav className="flex gap-4">
+        <nav className="flex gap-4 font-bold">
           <Link
             href={"/members/"}
             className="text-white hover:text-yellow-300 transition duration-300"
