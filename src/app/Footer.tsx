213065050e5e050bda88d71bf1ebee27a060b061
rei <idareiya123@gmail.com>
2024-09-24 20:17:08 +0900

    edit:画像並べる

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 function Footer() {
   return (
     <footer className="p-4 bg-gradient-to-r from-yellow-500 to-purple-600">
-      <div>
-        <p className="text-center text-white text-sm md:text-base">©2024 魔神の王国 - reicode</p>
+      <div className="text-center text-white text-sm md:text-base flex justify-center items-center">
+        <span className="mr-2">©2024 魔神の王国 - reicode</span>
+        <Image
+          src={"/rei.png"}
+          alt={"reiの画像"}
+          className=""
+          width={25}
+          height={25}
+        />
       </div>
     </footer>
   );
