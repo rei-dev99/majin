e8c389e245499de60958e9c7bc8594c35c667dda
rei <idareiya123@gmail.com>
2024-09-25 13:05:05 +0900

    edit:ogp設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,21 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "🧀🐮魔神クエスト",
   description: "このアプリは魔神が独断と偏見で仲良しメンバーをクイズで紹介するプラットフォームです。",
+  openGraph: {
+    title: '🧀🐮魔神クエスト',
+    description: 'このアプリは魔神が独断と偏見で仲良しメンバーをクイズで紹介するプラットフォームです。',
+    url: 'https://majin-r821.vercel.app/',
+    siteName: '🧀🐮魔神クエスト',
+    images: [
+      {
+        url: 'https://github.com/user-attachments/assets/5b449d42-42bb-437b-84e4-a6b95df8bf7f',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'ja_JP',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
