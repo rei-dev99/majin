881ad9e0690fa55aeca794f4dc00853f7f2df769
rei <idareiya123@gmail.com>
2024-09-27 21:02:54 +0900

    edit:コンポーネントファイル読み込み

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import AboutContent from "../components/about/AboutContent";
 
 export default function About() {
   return (
     <div className="w-full bg-gradient-to-b from-blue-200 to-blue-400 p-6 min-h-screen">
       <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-4">
-        魔神の紹介ページへようこそ！<br />ここで魔神の秘密を暴露しちゃうぞ！
+        魔神の紹介ページへようこそ！
+        <br />
+        ここで魔神の秘密を暴露しちゃうぞ！
       </h1>
       <p className="text-base md:text-lg font-bold text-center text-gray-100 mb-6">
         ここは魔神の世界を知るための特別な場所だ。君も魔神に染まるかもね！
@@ -18,32 +21,7 @@ export default function About() {
           height={500}
         />
       </div>
-      <div className="flex flex-col md:flex-row justify-around gap-4">
-        <div className="bg-white p-4 rounded shadow-md w-full md:w-1/3">
-          <h2 className="text-xl md:text-2xl font-semibold text-purple-700">
-            最近の調子
-          </h2>
-          <p className="text-gray-700 mt-2">
-            最近は調子がすこぶるいい！なんてのは嘘だ。技術に関心はあるものの、AIに頼らないと実装が怪しい。だけど、このプラットフォームには自分なりに仮説を立てて、ReactのHookも説明できる範囲で使ったぞ！技術よりも大事なこともあるから、そちらも全力で頑張るつもりだ！
-          </p>
-        </div>
-        <div className="bg-white p-4 rounded shadow-md w-full md:w-1/3">
-          <h2 className="text-xl md:text-2xl font-semibold text-purple-700">
-            魔神の秘話
-          </h2>
-          <p className="text-gray-700 mt-2">
-          最初にいちご🍓スタンプを始めたのは俺じゃない。俺のモコタン師匠からだ！それを知名人に送りまくったら、いつの間にか話題になってたんだ！そんなこんなで毎日スタンプを送って反応を楽しんでいたら、いつの間にか魔神になっていたわけさ！
-          </p>
-        </div>
-        <div className="bg-white p-4 rounded shadow-md w-full md:w-1/3">
-          <h2 className="text-xl md:text-2xl font-semibold text-purple-700">
-            開発目的と技術
-          </h2>
-          <p className="text-gray-700 mt-2">
-          技術検証とアウトプット目的で「React」「Next.js」「TailwindCSS」、「Supabase」「Vercel」を選んだぞ！Supabaseなら無料でDBを使えるからお得だ。実は1問だけじゃなく、2問3問とたくさん作りたかったんだけど、期間の都合でカットしたんだ。今後時間があればもっとクイズを作りたいし、みんなが自由に投稿できるようにしたいな！
-          </p>
-        </div>
-      </div>
+      <AboutContent />
     </div>
   );
 }
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,56 +1,14 @@
-"use client";
-
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { Members } from "@/types";
+import MemberContent from "../components/member/MemberContent";
 
 export default function MembersPage() {
-  // TypeScriptのジェネリクスを使って、membersの状態がMembers型の配列であることを指定、([]):useStateの初期値として空の配列を設定
-  const [members, setMembers] = useState<Members[]>([]);
-
-  useEffect(() => {
-    const fetchMembers = async () => { // asyncとfetchを使用することで非同期処理を実現している。
-      const response = await fetch("/api/members");
-      const data = await response.json();
-      setMembers(data)
-    }
-    fetchMembers();
-  },[])
-
   return (
     <div className="min-h-screen p-6 bg-gradient-to-b from-purple-200 to-purple-400">
       <h1 className="text-2xl md:text-4xl font-bold text-center text-white mb-6">
-        勇者たちのクイズを<br className="block md:hidden" />選んでください
+        勇者たちのクイズを
+        <br className="block md:hidden" />
+        選んでください
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {members.map((member) => (
-          <div
-            key={member.id}
-            className="bg-white p-4 rounded shadow text-center"
-          >
-            {/* 画像表示 */}
-            <Image
-              src={member.picture ? member.picture : "/monster.png"} // 画像がnullの場合デフォルト画像を表示
-              alt={`${member.name} の画像`}
-              className="rounded-full mb-2 mx-auto"
-              width={128} // 画像の幅
-              height={128} // 画像の高さ
-            />
-            {/* 名前表示 */}
-            <h2 className="text-xl md:text-2xl font-semibold text-purple-700 mb-4">
-              {member.name}
-            </h2>
-            {/* クイズに挑戦ボタン */}
-            <Link
-              href={`/members/${member.id}`}
-              className="text-white bg-blue-600 hover:bg-blue-700 rounded md:px-4 px-2 py-2 inline-block text-sm md:text-base font-bold"
-            >
-              クイズに挑戦
-            </Link>
-          </div>
-        ))}
-      </div>
+      <MemberContent />
     </div>
   );
 }
